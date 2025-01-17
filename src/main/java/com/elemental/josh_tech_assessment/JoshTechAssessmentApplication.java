b5c91392c68800bc729f63957b03a80121059e90
Josh Williamson <williamsonjdw@hotmail.co.uk>
2025-01-17 14:02:08 +0000

    initialising repo

---
+++ src/main/java/com/elemental/josh_tech_assessment/JoshTechAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.elemental.josh_tech_assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoshTechAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoshTechAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elemental/josh_tech_assessment/JoshTechAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elemental.josh_tech_assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoshTechAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
