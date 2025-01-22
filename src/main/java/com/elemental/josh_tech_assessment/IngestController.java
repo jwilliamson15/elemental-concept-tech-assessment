ef2a816a416dc614b7a4bb19d7ede17a387e1a15
Ener <ne00115@surrey.ac.uk>
2025-01-22 04:52:59 +0000

    Introducing controller, entity and repository

---
+++ src/main/java/com/elemental/josh_tech_assessment/IngestController.java
@@ -0,0 +1,13 @@
+package com.elemental.josh_tech_assessment;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IngestController {
+
+    @PostMapping("/upload")
+    public void ingestFile() {
+        //TODO
+    }
+}
---
+++ src/main/java/com/elemental/josh_tech_assessment/TravellerRepository.java
@@ -0,0 +1,6 @@
+package com.elemental.josh_tech_assessment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TravellerRepository  extends JpaRepository<Traveller, Long> {
+}
